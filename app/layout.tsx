1430ac2d8bb25c44a808b9032bc6e6258869c723
chandn0 <boinapallychandan@gmail.com>
2024-03-09 18:36:51 +0530

    removed alt

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" suppressHydrationWarning>
-			<Head> <meta property="og:image:alt" content={`${modular}`} /></Head>
+			<Head> <meta property="og:image" content={`${modular}`} /></Head>
 			
 			{/* <head /> */}
 			<body
